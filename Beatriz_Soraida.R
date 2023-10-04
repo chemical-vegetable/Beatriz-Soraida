c9c565ad30955802a55b1f1210028465f7b51d48
Sgarcia86 <sgarci86@uic.edu>
2023-10-04 13:59:44 -0700

    Data download

---
+++ Beatriz_Soraida.R
@@ -0,0 +1,5 @@
+
+
+
+lobster_abundace<-read.csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sbc.77.8&entityid=f32823fba432f58f66c06b589b7efac6")
+lobster_traps<-read.csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sbc.77.8&entityid=66dd61c75bda17c23a3bce458c56ed84")
